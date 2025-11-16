bcf8487952c390b619f77b21b58cfb985e1a3c8b
sidduneraki <sidduneraki77@gmail.com>
2025-11-16 16:27:44 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+        pathname: '/image/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
